a3b276fbeb1a605e5bf1dd56ee5b5273c221affa
numberx <shekooha696@gmail.com>
2022-02-27 17:58:32 +0330

    Fixed routing issues

--- ahang/urls.py
+++ ahang/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name='music'
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^upload$', views.upload_music_form, name='upload'),
-    url(r'^search$', views.search, name='search'),
-    url(r'^signup$', views.register, name='register'),
-    url(r'^login$', views.login_view, name='login'),
-    url(r'^logout$', views.logout_user, name='logout'),
+    url(r'^upload/$', views.upload_music_form, name='upload'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^signup/$', views.register, name='register'),
+    url(r'^login/$', views.login_view, name='login'),
+    url(r'^logout/$', views.logout_user, name='logout'),
     url(r'^edame/(?P<id>[0-9]{1,1000})*/$', views.ahang_detail, name='detail'),
 ]
 
