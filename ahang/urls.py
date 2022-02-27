709693fd7f4d788e1bb9af9b8fc4bf8ca1410778
numberx <shekooha696@gmail.com>
2022-02-28 00:15:15 +0330

    Upgraded to django 4

--- ahang/urls.py
+++ ahang/urls.py
@@ -1,15 +1,16 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 app_name='music'
 urlpatterns = [
-    url(r'^$', views.index, name='home'),
-    url(r'^upload/$', views.upload_music_form, name='upload'),
-    url(r'^search/$', views.search, name='search'),
-    url(r'^signup/$', views.register, name='register'),
-    url(r'^login/$', views.login_view, name='login'),
-    url(r'^logout/$', views.logout_user, name='logout'),
-    url(r'^edame/(?P<id>[0-9]{1,1000})*/$', views.ahang_detail, name='detail'),
+    re_path(r'^$', views.index, name='home'),
+    re_path(r'^upload/$', views.upload_music_form, name='upload'),
+    re_path(r'^search/$', views.search, name='search'),
+    re_path(r'^signup/$', views.register, name='register'),
+    re_path(r'^login/$', views.login_view, name='login'),
+    re_path(r'^logout/$', views.logout_user, name='logout'),
+    re_path(r'^edame/(?P<id>[0-9]{1,1000})*/$', views.ahang_detail, name='detail'),
+    # url(r'^edame/(?P<id>[0-9]{1,1000})*/$', views.ahang_detail, name='detail'),
 ]
 
 # url(r'^(?P<ahang_esm>[\w+][^/]*)$', views.ahang_detail, name='detail'),
