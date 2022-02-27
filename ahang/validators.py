008c648bc0f7390c7155a97df83c21e076526250
numberx <shekooha696@gmail.com>
2022-02-27 11:30:32 +0330

    Housekeeping

--- ahang/validators.py
+++ ahang/validators.py
@@ -1,7 +1,18 @@
 from django.core.exceptions import ValidationError
-
+exts = [
+        ".mp3",
+        ".wav",
+        ".pcm",
+        ".aiff",
+        ".aac",
+        ".ogg",
+        ".wma",
+        ".flac",
+        ".wma",
+        ]
 def validate_file(value):
     value = str(value)
-    if (value.endswith(".mp3") != True and value.endswith(".wav") != True and value.endswith(".pcm") != True and value.endswith(".aiff") != True and value.endswith(".aac") != True and value.endswith(".ogg") != True and value.endswith(".wma") != True and value.endswith(".flac") != True and value.endswith(".wma") != True):
-        raise ValidationError("ِYo this file format is not supported")
-    return value
+    for i in exts:
+         if (value.endswith(i) != True):
+            raise ValidationError('Yo this file format is not supported')
+         return value
